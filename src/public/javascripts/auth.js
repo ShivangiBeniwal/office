c0a516e6187668b22e41fa08b601ff9b1f326491
shivangi beniwal <shivi17ben@gmail.com>
2020-04-18 18:35:42 +0530

    added time to logs

--- src/public/javascripts/auth.js
+++ src/public/javascripts/auth.js
@@ -15,7 +15,7 @@
             },
         };
 
-        printLog("Get Auth Token Call made.");
+        printLog("Get Auth Token Call is made.");
         microsoftTeams.authentication.getAuthToken(authTokenRequest);
     }
 
@@ -81,11 +81,20 @@
 
     // ------------------------------------------------------------------------
 
+    function getCurrentDateTime() {
+        var today = new Date();
+        var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        return dateTime;
+    }
     function printLog(msg) {
+        var finalMessage = '['+getCurrentDateTime()+'] '+msg;
         var logDiv = document.getElementById('logs');
         var p = document.createElement("p");
-        logDiv.prepend(msg, p);
-        console.log("Auth: " + msg);
+        logDiv.prepend(finalMessage, p);
+        console.log("Auth: " + finalMessage);
     }
 
     function handleServerResponse(data) {
