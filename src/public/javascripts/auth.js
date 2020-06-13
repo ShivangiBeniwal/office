8542f19a0e82e77f6d0746cbc647314c6da59e2f
shivangi beniwal <shivi17ben@gmail.com>
2020-06-14 01:17:19 +0530

    modification in perf

--- src/public/javascripts/auth.js
+++ src/public/javascripts/auth.js
@@ -103,9 +103,9 @@
             startTime = new Date().getTime();
             printLogItem("StartTime = " + startTime);
 
-            for ( var i = 1; i <= maxCount; i++) {
-                getAuthTokenWithCount(i);
-            }
+            // for ( var i = 1; i <= maxCount; i++) {
+            //     getAuthTokenWithCount(i);
+            // }
         }
     }
 
@@ -113,13 +113,13 @@
         // Get auth token
         var authTokenRequest = {
             successCallback: (result) =>  {
-                // document.getElementById('countSuccess').innerHTML = "Total success calls - " + count;
+                document.getElementById('countSuccess').innerHTML = "Total success calls - " + count;
                 if (count == maxCount) {
                     printEndtime();
                 }
             },
             failureCallback: function(error) { 
-                // document.getElementById('countError').innerHTML = "Total error calls - " + count;
+                document.getElementById('countError').innerHTML = "Total error calls - " + count;
                 if (count == maxCount) {
                     printEndtime();
                 }
