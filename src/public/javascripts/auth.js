ffdc6e966737b361e7964d2a4d3021c2425bc1d3
shivangi beniwal <shivi17ben@gmail.com>
2020-06-14 02:48:25 +0530

    modification in perf

--- src/public/javascripts/auth.js
+++ src/public/javascripts/auth.js
@@ -101,9 +101,6 @@
 })();
 
 function hello() {
-    var today = new Date();
-    var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
-    console.log(dateTime);
+    var endTime = new Date().getTime();
+        printLogItem("EndTime = " + getCurrentDateTime());
 }
