fe9fb79d5cf601e91cf3bec6ca79d5703d4c8fb5
shivangi beniwal <shivi17ben@gmail.com>
2020-06-14 03:04:57 +0530

    modification in perf

--- src/public/javascripts/auth.js
+++ src/public/javascripts/auth.js
@@ -30,7 +30,7 @@
         logDiv.prepend(finalMessage, p);
         console.log("Auth : " + finalMessage);
     }
-    
+
     function getCurrentDateTime() {
         var today = new Date();
         var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
@@ -46,62 +46,62 @@
     
 })();
 
-function clickAuthPerf() {
-    var startTime;
-    var maxCount = 1000;
-    function checkPerformance() {
-        // var btn = document.getElementById("promptForAuthPerf");
-        // btn.onclick = () => {
-            maxCount = document.querySelector('input[name=countVal]').value;
-            startTime = new Date().getTime();
-            printLogItem("StartTime = " + getCurrentDateTime());
+// function clickAuthPerf() {
+//     var startTime;
+//     var maxCount = 1000;
+//     function checkPerformance() {
+//         // var btn = document.getElementById("promptForAuthPerf");
+//         // btn.onclick = () => {
+//             maxCount = document.querySelector('input[name=countVal]').value;
+//             startTime = new Date().getTime();
+//             printLogItem("StartTime = " + getCurrentDateTime());
 
-            for ( var i = 1; i <= maxCount; i++) {
-                getAuthTokenWithCount(i);
-            }
-        // }
-    }
+//             for ( var i = 1; i <= maxCount; i++) {
+//                 getAuthTokenWithCount(i);
+//             }
+//         // }
+//     }
 
-    function getAuthTokenWithCount(count) {
-        // Get auth token
-        var authTokenRequest = {
-            successCallback: (result) =>  {
-                document.getElementById('countSuccess').innerHTML = "Total success calls : " + count;
-                if (count == maxCount) {
-                    printEndtime();
-                }
-            },
-            failureCallback: function(error) { 
-                document.getElementById('countError').innerHTML = "Total error calls : " + count;
-                if (count == maxCount) {
-                    printEndtime();
-                }
-            },
-        };
-        microsoftTeams.authentication.getAuthToken(authTokenRequest);
-    }
+//     function getAuthTokenWithCount(count) {
+//         // Get auth token
+//         var authTokenRequest = {
+//             successCallback: (result) =>  {
+//                 document.getElementById('countSuccess').innerHTML = "Total success calls : " + count;
+//                 if (count == maxCount) {
+//                     printEndtime();
+//                 }
+//             },
+//             failureCallback: function(error) { 
+//                 document.getElementById('countError').innerHTML = "Total error calls : " + count;
+//                 if (count == maxCount) {
+//                     printEndtime();
+//                 }
+//             },
+//         };
+//         microsoftTeams.authentication.getAuthToken(authTokenRequest);
+//     }
 
-    function printEndtime() {
-        var endTime = new Date().getTime();
-        printLogItem("EndTime = " + getCurrentDateTime());
-        printLogItem("Total time taken = " + (endTime - startTime) + " ms\n");
-    }
+//     function printEndtime() {
+//         var endTime = new Date().getTime();
+//         printLogItem("EndTime = " + getCurrentDateTime());
+//         printLogItem("Total time taken = " + (endTime - startTime) + " ms\n");
+//     }
 
-    function printLogItem(text) {
-        var node = document.createElement("li");
-        var textNode = document.createTextNode(text);
-        node.appendChild(textNode);
-        document.getElementById('logItems').appendChild(node);
-    }
+//     function printLogItem(text) {
+//         var node = document.createElement("li");
+//         var textNode = document.createTextNode(text);
+//         node.appendChild(textNode);
+//         document.getElementById('logItems').appendChild(node);
+//     }
 
-    // ------------------------------------------------------------------------
+//     // ------------------------------------------------------------------------
 
-    function getCurrentDateTime() {
-        var today = new Date();
-        var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var dateTime = date+' '+time;
+//     function getCurrentDateTime() {
+//         var today = new Date();
+//         var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+//         var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//         var dateTime = date+' '+time;
 
-        return dateTime;
-    }
-}
+//         return dateTime;
+//     }
+// }
