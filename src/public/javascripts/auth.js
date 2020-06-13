82ca28314c6fa02402ca439b5d2d274cbe8464ad
shivangi beniwal <shivi17ben@gmail.com>
2020-06-13 23:56:47 +0530

    modification in perf

--- src/public/javascripts/auth.js
+++ src/public/javascripts/auth.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-    
+
 /* 
     // 1. Get auth token
     // Ask Teams to get us a token from AAD, we should exchange it when it n
@@ -157,10 +157,10 @@
         console.log("Auth: " + finalMessage);
     }
 
-    // initializeConsentButton();
+    initializeConsentButton();
 
     // getAuthToken();
 
-    // checkPerformance();
+    checkPerformance();
     
 })();
