549284e5acbe4d205955bc5dbf18ab0bd0e8cfea
shivangi beniwal <shivi17ben@gmail.com>
2020-06-14 03:03:01 +0530

    modification in perf

--- src/public/javascripts/auth.js
+++ src/public/javascripts/auth.js
@@ -30,10 +30,17 @@
         logDiv.prepend(finalMessage, p);
         console.log("Auth : " + finalMessage);
     }
+    
+    function getCurrentDateTime() {
+        var today = new Date();
+        var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        return dateTime;
+    }
 
     // ------------------------------------------------------------------------
-
-    
 
     initializeConsentButton();
     
