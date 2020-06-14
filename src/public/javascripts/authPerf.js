068be98b3cbefc9a041dd74fb76a898a7a9b03a4
shivangi beniwal <shivi17ben@gmail.com>
2020-06-14 17:03:40 +0530

    modification in perf

--- src/public/javascripts/authPerf.js
+++ src/public/javascripts/authPerf.js
@@ -1,94 +1,128 @@
 (function() {
-    "use strict";
+  "use strict";
 
-    var startTime;
+  var rowId = 0;
+  var startTime;
+  var countSuccess = 0;
+  var countError = 0;
 
-    var rowId = 0;
-  
-    var maxCount = 1000;
-  
-    function checkPerformance() {
-      var btn = document.getElementById("promptForAuthPerf");
-      btn.onclick = () => {
-        createNewRow();
-        maxCount = document.querySelector("input[name=countVal]").value;
+  var maxCount = 1000;
+  var batchCount = maxCount;
+  var batchInterval = 0;
 
-        startTime = new Date().getTime();
-        document.getElementById('startTime'+rowId).innerHTML = getCurrentDateTime();
-  
-        for (var i = 1; i <= maxCount; i++) {
-          getAuthTokenWithCount(i);
+  function checkPerformance() {
+    var btn = document.getElementById("promptForAuthPerf");
+    btn.onclick = () => {
+      createNewRow();
+      init();
+
+      for (var i = 1, ct = 1; i <= maxCount; i++, ct++) {
+        if (ct == batchCount) {
+          wait(batchInterval);
+          ct = 1;
         }
-      };
+        getAuthTokenWithCount(i);
+      }
+    };
+  }
+
+  function createNewRow() {
+    var tableBody = document.getElementById("resultTableBody");
+    var row = document.createElement("tr");
+    row.id = rowId;
+
+    row.appendChild(createCell("queryDetails" + rowId));
+    row.appendChild(createCell("totalCount" + rowId));
+    row.appendChild(createCell("startTime" + rowId));
+    row.appendChild(createCell("endTime" + rowId));
+    row.appendChild(createCell("totalTime" + rowId));
+
+    tableBody.insertBefore(row, tableBody.firstChild);
+  }
+
+  function createCell(cellId) {
+    var cell = document.createElement("td");
+    cell.id = cellId;
+    cell.innerHTML = "-";
+
+    return cell;
+  }
+
+  function init() {
+    var totalCount = document.querySelector("input[name=countVal]").value;
+    var bCount = document.querySelector("input[name=batchCount]").value;
+    var bInterval = document.querySelector("input[name=batchInterval]").value;
+
+    maxCount = totalCount > 0 ? totalCount : maxCount;
+    batchCount = bCount > 0 ? bCount : batchCount;
+    batchInterval = bInterval > 0 ? bInterval : batchInterval;
+    document.getElementById("queryDetails" + rowId).innerHTML = maxCount + " [ " + batchCount + " - " + batchInterval + " ms]";
+
+    countSuccess = 0;
+    countError = 0;
+
+    startTime = new Date().getTime();
+    document.getElementById("startTime" + rowId).innerHTML = getCurrentDateTime();
+  }
+
+  function wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+      end = new Date().getTime();
     }
+  }
 
-    function createNewRow() {
-        var tableBody = document.getElementById('resultTableBody');
-        var row = document.createElement('tr');
-        row.id = rowId;
+  function getAuthTokenWithCount(count) {
+    // Get auth token
+    var authTokenRequest = {
+      successCallback: result => {
+        countSuccess++;
+        printCallCount();
 
-        row.appendChild(createCell('countSuccess'+rowId));
-        row.appendChild(createCell('countError'+rowId));
-        row.appendChild(createCell('startTime'+rowId));
-        row.appendChild(createCell('endTime'+rowId));
-        row.appendChild(createCell('totalTime'+rowId));
-
-        tableBody.insertBefore(row, tableBody.firstChild);
-    }
-
-    function createCell(cellId) {
-        var cell = document.createElement('td');
-        cell.id = cellId;
-        cell.innerHTML = "-"; 
-
-        return cell;
-    }
-  
-    function getAuthTokenWithCount(count) {
-      // Get auth token
-      var authTokenRequest = {
-        successCallback: result => {
-          document.getElementById('countSuccess'+rowId).innerHTML = count;
-  
-          if (count == maxCount) {
-            printEndtime();
-          }
-        },
-        failureCallback: function(error) {
-          document.getElementById('countError'+rowId).innerHTML = count;
-  
-          if (count == maxCount) {
-            printEndtime();
-          }
+        if (count == maxCount) {
+          printEndtime();
         }
-      };
-      microsoftTeams.authentication.getAuthToken(authTokenRequest);
-    }
-  
-    function printEndtime() {
-      var endTime = new Date().getTime();
-      document.getElementById('endTime'+rowId).innerHTML = getCurrentDateTime();
-      document.getElementById('totalTime'+rowId).innerHTML = (endTime - startTime) + " ms";
-      rowId++;
-    }
-  
-    // ------------------------------------------------------------------------
-  
-    function getCurrentDateTime() {
-      var today = new Date();
-      var date =
-        today.getDate() +
-        "/" +
-        (today.getMonth() + 1) +
-        "/" +
-        today.getFullYear();
-      var time =
-        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-      var dateTime = date + " " + time;
-  
-      return dateTime;
-    }
-  
-    checkPerformance();
-  })();
-  
+      },
+      failureCallback: function(error) {
+        countError++;
+        printCallCount();
+
+        if (count == maxCount) {
+          printEndtime();
+        }
+      }
+    };
+    microsoftTeams.authentication.getAuthToken(authTokenRequest);
+  }
+
+  function printCallCount() {
+    document.getElementById("totalCount" + rowId).innerHTML = "(" + countSuccess + "/" + countError + ")";
+  }
+
+  function printEndtime() {
+    var endTime = new Date().getTime();
+    document.getElementById("endTime" + rowId).innerHTML = getCurrentDateTime();
+    document.getElementById("totalTime" + rowId).innerHTML = endTime - startTime + " ms";
+    rowId++;
+  }
+
+  // ------------------------------------------------------------------------
+
+  function getCurrentDateTime() {
+    var today = new Date();
+    var date =
+      today.getDate() +
+      "/" +
+      (today.getMonth() + 1) +
+      "/" +
+      today.getFullYear();
+    var time =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + " " + time;
+
+    return dateTime;
+  }
+
+  checkPerformance();
+})();
