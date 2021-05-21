a38fafcfbc8548e2ebd3da7c8aec115250911cb4
shivangi beniwal <shivi17ben@gmail.com>
2021-05-21 17:49:14 +0530

    fixed file path

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { initializeDCP } from './public/scripts/deviceCapabilty';
-import { initializeAuth } from './public/scripts/auth';
+// import { initializeAuth } from './public/scripts/auth';
 // import { initializeDCP } from './public/scripts/deviceCapabilty';
 
 (function () {
     initializeDCP();
-    initializeAuth();
+    // initializeAuth();
 })();
