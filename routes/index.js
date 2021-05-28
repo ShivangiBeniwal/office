77831b5ae57648aba06cb5e6277527c493fa3e70
shivangi beniwal <shivi17ben@gmail.com>
2021-05-28 11:03:18 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test Auth' });
+  // res.render('index', { title: 'Test Auth' });
+  res.sendFile(path.join(__dirname, "../src/views/index.html"));
 });
 
 module.exports = router;
