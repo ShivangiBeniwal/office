13b2804550b93ae56ace36e39540e9946eb6b4c2
Meeth <meeth.jain@microsoft.com>
2022-02-23 13:49:22 +0530

    Navigation Changes

--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,15 @@
 import { initializeDCP } from './scripts/deviceCapabilty';
+import { initializeNavigation } from './scripts/navigation'
 import { printLog } from './utils/utils';
 
 export const initialize = () => {
     const logTag = "init"
-
-    initializeDCP()
-    printLog(logTag, "initializeDCP")
+    const path = window.location.pathname
+    
+    if (path.includes("navigation")) {
+        initializeNavigation()
+    } else {
+        initializeDCP()
+        printLog(logTag, "initializeDCP")
+    }
 }
