5dff1bd8e581ba5cf6c676a3c5aab080b1da1390
shbeniwa <shbeniwa@microsoft.com>
2022-03-14 11:47:44 +0530

    removed unused code

--- src/scripts/cowatch.js
+++ src/scripts/cowatch.js
@@ -1,14 +1,13 @@
 (function () {
 
-    // Call the initialize API first
-    microsoftTeams.initialize();
-
     var start = document.getElementById("start")
     start.onclick = () => {
         if (false) {
             window.open("http://localhost:3001");
         } else {
             var appContentUrl = 'https://devicecapabilitytestapp.azurewebsites.net/cowatch';
+            console.log(">>> initialize");
+            microsoftTeams.initialize();
             console.log(">>> Start click %s", appContentUrl);
             microsoftTeams.meeting.shareAppContentToStage((error, result) => {
                 console.log(">>> Inside shareAppContentToStage %s, %s", JSON.stringify(error), result);
