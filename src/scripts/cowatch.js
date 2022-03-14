4ed3ba4549ee3047a22f28c2d96f86fd4f5d7097
shbeniwa <shbeniwa@microsoft.com>
2022-03-14 12:59:44 +0530

    added context

--- src/scripts/cowatch.js
+++ src/scripts/cowatch.js
@@ -1,13 +1,19 @@
 (function () {
 
+    console.log(">>> initialize");
+    microsoftTeams.initialize();
+
+    console.log(">>> getContext");
+    microsoftTeams.getContext(context => {
+        console.log(">>> Inside getContext %s", JSON.stringify(context));
+    });
+
     var start = document.getElementById("start")
     start.onclick = () => {
         if (false) {
             window.open("http://localhost:3001");
         } else {
             var appContentUrl = 'https://devicecapabilitytestapp.azurewebsites.net/cowatch';
-            console.log(">>> initialize");
-            microsoftTeams.initialize();
             console.log(">>> Start click %s", appContentUrl);
             microsoftTeams.meeting.shareAppContentToStage((error, result) => {
                 console.log(">>> Inside shareAppContentToStage %s, %s", JSON.stringify(error), result);
