003bd84ef538bd0e47ce505302526da26e8ef232
shbeniwa <shbeniwa@microsoft.com>
2022-03-22 17:19:50 +0530

    added input box for sharetostage

--- src/scripts/cowatch.js
+++ src/scripts/cowatch.js
@@ -11,10 +11,16 @@
 
     var start = document.getElementById("start")
     start.onclick = () => {
-        var appContentUrl = "https://3efc-139-5-254-252.ngrok.io/#/youtube?providerId=youtube001&organizer=689c4267-cc0c-453d-b730-a4b1a957a2ac&userRole=organizer";
-        printLog(logTag, `>>> Start click ${appContentUrl}`);
-        microsoftTeams.meeting.shareAppContentToStage((error, result) => {
-            printLog(logTag, `>>> Inside shareAppContentToStage ${JSON.stringify(error)}, ${result}`);
-        }, appContentUrl);
+        const appContentUrl = "https://3efc-139-5-254-252.ngrok.io/#/youtube?providerId=youtube001&organizer=689c4267-cc0c-453d-b730-a4b1a957a2ac&userRole=organizer";
+        var ShareToStage = document.querySelector("input[name=ShareToStage]").value;
+
+        printLog(logTag, `Selected value for ShareToStage is ${!!Boolean(ShareToStage)}`)
+        if (!!Boolean(ShareToStage)) {
+            window.open(appContentUrl);
+        } else {
+            microsoftTeams.meeting.shareAppContentToStage((error, result) => {
+                printLog(logTag, `>>> Inside shareAppContentToStage ${JSON.stringify(error)}, ${result}`);
+            }, appContentUrl);
+        }
     }
 })();
