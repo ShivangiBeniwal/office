ea241988320077d528e412abc3b4ad33b0931e95
shbeniwa <shbeniwa@microsoft.com>
2022-03-22 16:19:44 +0530

    Added Log

--- src/scripts/cowatch.js
+++ src/scripts/cowatch.js
@@ -1,24 +1,20 @@
 (function () {
 
-    console.log(">>> initialize");
+    const logTag = "CoWatch";
+    printLog(logTag, ">>> initialize");
     microsoftTeams.initialize();
 
-    console.log(">>> getContext");
+    printLog(logTag, ">>> getContext");
     microsoftTeams.getContext(context => {
-        console.log(">>> Inside getContext %s", JSON.stringify(context));
+        printLog(logTag, ">>> Inside getContext %s", JSON.stringify(context));
     });
 
     var start = document.getElementById("start")
     start.onclick = () => {
-        if (false) {
-            window.open("https://329d-139-5-254-252.ngrok.io/");
-        } else {
-            var appContentUrl = "https://329d-139-5-254-252.ngrok.io/";
-            console.log(">>> Start click %s", appContentUrl);
-            microsoftTeams.meeting.shareAppContentToStage((error, result) => {
-                console.log(">>> Inside shareAppContentToStage %s, %s", JSON.stringify(error), result);
-            }, appContentUrl);
-        }
+        var appContentUrl = "https://3efc-139-5-254-252.ngrok.io/#/youtube?providerId=youtube001&organizer=689c4267-cc0c-453d-b730-a4b1a957a2ac&userRole=organizer";
+        printLog(logTag, ">>> Start click %s", appContentUrl);
+        microsoftTeams.meeting.shareAppContentToStage((error, result) => {
+            printLog(logTag, ">>> Inside shareAppContentToStage %s, %s", JSON.stringify(error), result);
+        }, appContentUrl);
     }
-
 })();
